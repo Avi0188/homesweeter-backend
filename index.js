2190a52e295e7252e8e3fe1411e1cdb89d1c13c6
unknown <rai2889@gmail.com>
2023-08-27 14:06:46 +0530

    full backend

--- index.js
+++ index.js
@@ -2,26 +2,21 @@ const express=require("express");
 const { connection } = require("./db");
 const {propertyRouter}=require("./routes/property.route");
 const { userRouter } = require("./routes/user.route");
+const { adminRoutes } = require("./routes/admin.routes");
 const { orders } = require("./controller/paymentController");
 const { verify } = require("jsonwebtoken");
-// const { adminuserRouter } = require("./routes/adminuser.route");
-require("dotenv").config();
+
 const cors=require("cors")
+require("dotenv").config();
 
 const app=express();
 
 app.use(cors());
 app.use(express.json());
-app.get("/",(req,res)=>{
-try {
-    return res.status(200).send("Sweeter-Home")
-} catch (error) {
-    return res.status(400).send({error:error.message})
-}
-})
+
 app.use("/property",propertyRouter);
 app.use("/users",userRouter)
-// app.use("/admins",adminuserRouter)
+app.use("/admins",adminRoutes)
 app.post("/orders",orders)
 app.post("/verify",verify);
 app.listen(process.env.PORT,async()=>{
